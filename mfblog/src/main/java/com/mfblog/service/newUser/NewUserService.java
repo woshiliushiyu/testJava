52ad122f903ab65fde8bbc590641a8a26a8a2db2
刘世玉 <1010530278@qq.com>
2017-06-30 19:03:24 +0800

    no message

---
+++ mfblog/src/main/java/com/mfblog/service/newUser/NewUserService.java
@@ -0,0 +1,25 @@
+package com.mfblog.service.newUser;
+
+import com.mfblog.domain.TestNewUser;
+import com.mfblog.service.BaseService;
+import org.apache.ibatis.javassist.NotFoundException;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Created by liushiyu on 2017/6/30.
+ */
+public interface NewUserService extends BaseService<TestNewUser> {
+
+    @Override
+    void add(TestNewUser testNewUser) throws Exception;
+
+    @Override
+    List<TestNewUser> findAll(int pageNum, int pageSize);
+
+    @Override
+    TestNewUser findOneById(Serializable id) throws NotFoundException;
+
+    void del(String name) throws Exception;
+}
---
+++ mfblog/src/main/java/com/mfblog/service/newUser/NewUserServiceImpl.java
@@ -0,0 +1,42 @@
+package com.mfblog.service.newUser;
+
+import com.mfblog.dao.TestNewDao;
+import com.mfblog.domain.TestNewUser;
+import org.apache.ibatis.javassist.NotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Created by liushiyu on 2017/6/30.
+ */
+@Service("newUserService")
+public class NewUserServiceImpl implements NewUserService {
+
+    @Autowired
+    private TestNewDao testNewDao;
+
+    @Override
+    public void add(TestNewUser testNewUser) throws Exception {
+
+        testNewDao.add(testNewUser);
+    }
+
+    @Override
+    public List<TestNewUser> findAll(int pageNum, int pageSize) {
+        return null;
+    }
+
+    @Override
+    public TestNewUser findOneById(Serializable id) throws NotFoundException {
+        return null;
+    }
+
+    @Override
+    public void del(String name) throws Exception {
+        TestNewUser testNewUser = new TestNewUser(name,"","");
+        testNewDao.del(testNewUser);
+    }
+}
